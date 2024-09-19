cab16f76262bf0c01449b134e03b5480407901ff
divejane <sonny.nichols5689@gmail.com>
2024-09-19 12:08:31 -0500

    googoo gaga

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/teleop.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class teleop extends LinearOpMode {
@@ -12,10 +13,15 @@ public class teleop extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         // HARDWARE
 
-        DcMotor fL = hardwareMap.dcMotor.get("frontLeft");
-        DcMotor bL = hardwareMap.dcMotor.get("backLeft");
-        DcMotor fR = hardwareMap.dcMotor.get("frontRight");
-        DcMotor bR = hardwareMap.dcMotor.get("backRight");
+        DcMotor fL  = hardwareMap.dcMotor.get("frontLeft");
+        DcMotor bL  = hardwareMap.dcMotor.get("backLeft");
+        DcMotor fR  = hardwareMap.dcMotor.get("frontRight");
+        DcMotor bR  = hardwareMap.dcMotor.get("backRight");
+
+        Servo lClaw = hardwareMap.servo.get("lClaw");
+        Servo rClaw = hardwareMap.servo.get("rClaw");
+
+        DcMotor armLift = hardwareMap.dcMotor.get("armLift");
 
         fL.setDirection(DcMotorSimple.Direction.REVERSE);
         bL.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -46,6 +52,24 @@ public class teleop extends LinearOpMode {
             fR.setPower(fRPower);
             bR.setPower(bRPower);
 
+
+            // p2
+
+            armLift.setPower(gamepad2.left_stick_y);
+
+            if (gamepad2.a) {
+
+                lClaw.setPosition(1);
+                rClaw.setPosition(1);
+
+            }
+
+            if (gamepad2.b) {
+
+                lClaw.setPosition(0);
+                rClaw.setPosition(0);
+
+            }
         }
     }
 }
